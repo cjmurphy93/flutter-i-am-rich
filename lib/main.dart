8491f9a8a1c898878382e52a309349451710c35b
cjmurphy93 <cjmurph93@gmail.com>
2021-05-02 23:40:50 -0400

    Setup Basic image app

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,20 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Center(
-        child: Text('Hello World'),
+      home: Scaffold(
+        backgroundColor: Colors.white70,
+        appBar: AppBar(
+          title: Center(
+            child: Text('I Am Rich'),
+          ),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: Center(
+          child: Image(
+            image: NetworkImage(
+                'https://memegenerator.net/img/instances/50071351/jewels.jpg'),
+          ),
+        ),
       ),
     ),
   );
